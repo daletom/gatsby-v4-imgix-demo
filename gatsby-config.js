83ff676a75cdf1b0da0d1766ad0751185f702d79
sherwinski <sherwin@imgix.com>
2021-12-10 17:37:57 -0800

    config: add @imgix/gatsby config to plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const { ImgixSourceType } = require('@imgix/gatsby');
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -34,5 +36,31 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `@imgix/gatsby`,
+      options: {
+        // This is the domain of your imgix source, which can be created at
+        // https://dashboard.imgix.com/.
+        // Only "Web Proxy" imgix sources can be used for this configuration.
+        domain: 'sherwinski-proxy.imgix.net',
+
+        // This is the source's secure token. Can be found under the "Security"
+        // heading in your source's configuration page, and revealed by tapping
+        // "Show Token".
+        secureURLToken: 'abc123', // replace this token
+
+        // This configures the plugin to work in proxy mode.
+        sourceType: ImgixSourceType.WebProxy,
+
+        // These are some default imgix parameters to set for each image. It is
+        // recommended to have at least this minimal configuration.
+        defaultImgixParams: { auto: 'format,compress' },
+
+        // This configures which nodes to modify.
+        fields: [
+          // Add an object to this array for each node type you want to modify. Follow the instructions below for this.
+        ],
+      },
+    },
   ],
 }
