fa4aaefbe2fbb64582057ba961f94dd4988b379b
Tom Dale <thomas.dale2@gmail.com>
2021-12-11 09:10:41 -0800

    update readme and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
       Built with <a href="https://contentful.com/">Contentful</a> &middot;{' '}
       <a href="https://docs.imgix.com/libraries/gatsby">imgix</a> &middot;{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <a href="https://github.com/daletom/gatsby-v4-imgix-demo">Source</a>
     </div>
   </Container>
 )
